cd5727eccfb70266ebac873baedbc05d5e05a8f4
MSRbuddy <meghanadasari222@gmail.com>
2023-02-22 15:20:01 +0530

    User-Registration UC-1 is added!

---
+++ UR-UC1/UR-UC1/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UR_UC1;
+
+
+public class User
+{
+    public string FirstName
+    {
+        get;
+        set;
+    }
+    public User(string firstname)
+    {
+        this.FirstName = @"^[A-Z][a-zA-Z]{3,20}$";
+    }
+    public void printFirstName()
+    {
+        Console.WriteLine("FirstName is :{0}", this.FirstName);
+    }
+}
