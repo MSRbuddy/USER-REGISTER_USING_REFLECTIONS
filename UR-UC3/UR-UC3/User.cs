52991e270bcc8fbdf4b9f5bc77d39f8bf43b1057
MSRbuddy <meghanadasari222@gmail.com>
2023-02-22 15:24:55 +0530

    User-Registration UC-3 is added!

---
+++ UR-UC3/UR-UC3/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UR_UC3;
+
+
+public class User
+{
+    public string Email
+    {
+        get;
+        set;
+    }
+    public User(string email)
+    {
+        this.Email = @"^[a-z|a-z.a-z]@[a-z|a-z.a-z]|[.a-z]$";
+    }
+    public void printEmail()
+    {
+        Console.WriteLine("Email is :{0}", this.Email);
+    }
+}
