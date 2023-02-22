a2182bad96cdb6dc22a722ca8ea094d0fba16c06
MSRbuddy <meghanadasari222@gmail.com>
2023-02-22 15:36:42 +0530

    User-Registration UC-8 is added!

---
+++ UR-UC8/UR-UC8/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UR_UC8;
+
+public class User
+{
+    public string PwdRule4
+    {
+        get;
+        set;
+    }
+    public User(string pwdrule4)
+    {
+        this.PwdRule4 = @"^[A-Z]{1,7}[a-z]{6,20}[0-9]{1,7}[-\@\#\$\.\%\&\*]?$";
+    }
+    public void printPwdRule4()
+    {
+        Console.WriteLine("Password is :{0}", this.PwdRule4);
+    }
+}
