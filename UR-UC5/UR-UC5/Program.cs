c1123125ef595add67296b42fb3b70434677e153
MSRbuddy <meghanadasari222@gmail.com>
2023-02-22 15:32:14 +0530

    User-Registration UC-5 is added!

---
+++ UR-UC5/UR-UC5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+
+namespace UR_UC5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to UseCase-5!");
+
+            Type type = Type.GetType("User");
+
+            Console.WriteLine("Class name is :{0}", type.Name);
+
+            Console.WriteLine("Methods in User Class : ");
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+            }
+
+            Console.WriteLine("Properties in User Class : ");
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType.Name + " " + property.Name);
+            }
+
+            Console.WriteLine("Constructors in User Class : ");
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+        }
+    }
+}
---
+++ UR-UC5/UR-UC5/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UR_UC5;
+
+public class User
+{
+    public string PwdRule1
+    {
+        get;
+        set;
+    } 
+    public User(string pwdrule1)
+    {
+        this.PwdRule1 = @"^[A-Za-z]{8,20}$";
+    }
+    public void printPwdRule1()
+    {
+        Console.WriteLine("Password is :{0}", this.PwdRule1);
+    }
+}
