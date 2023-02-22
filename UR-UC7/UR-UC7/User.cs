fcd80126457da4d2bc0f270664f15c24ed5d76cd
MSRbuddy <meghanadasari222@gmail.com>
2023-02-22 15:35:09 +0530

    User-Registration UC-7 is added!

---
+++ UR-UC7/UR-UC7/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UR_UC7;
+
+public class User
+{
+    public string PwdRule3
+    {
+        get;
+        set;
+    }
+    public User(string pwdrule3)
+    {
+        this.PwdRule3 = @"^[A-Z]{1,7}[a-z]{6,20}[0-9]{1,7}$";
+    }
+    public void printPwdRule3()
+    {
+        Console.WriteLine("Password is :{0}", this.PwdRule3);
+    }
+}
