764c82c66f5484eb6b5000c1593f64c3171e8405
MSRbuddy <meghanadasari222@gmail.com>
2023-02-22 15:22:50 +0530

    User-Registration UC-2 is added!

---
+++ UR-UC2/UR-UC2/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UR_UC2;
+
+
+public class User
+{
+    public string LastName
+    {
+        get;
+        set;
+    }
+    public User(string lastname)
+    {
+        this.LastName = @"^[A-Z][a-zA-Z]{3,20}$";
+    }
+    public void printLastName()
+    {
+        Console.WriteLine("LastName is :{0}", this.LastName);
+    }
+}
