a4db66123181d6b4d62d7f67dbf5ed295a69a7fa
MSRbuddy <meghanadasari222@gmail.com>
2023-02-22 15:37:52 +0530

    User-Registration UC-9 is added!

---
+++ UR-UC9/UR-UC9/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UR_UC9;
+
+public class User
+{
+    public string SampleEmail
+    {
+        get;
+        set;
+    }
+    public User(string sampleemail)
+    {
+        this.SampleEmail = @"^[a-z|a-z.a-z]@[a-z|a-z.a-z]|[.a-z]$";
+    }
+    public void printSampleEmail()
+    {
+        Console.WriteLine("Email is :{0}", this.SampleEmail);
+    }
+}
