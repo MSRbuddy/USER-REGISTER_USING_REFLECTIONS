aabfba8175ff83a461a66881832b8848ed1b6365
MSRbuddy <meghanadasari222@gmail.com>
2023-02-22 15:26:36 +0530

    User-Registration UC-4 is added!

---
+++ UR-UC4/UR-UC4/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UR_UC4;
+
+
+public class User
+{
+    public string PhoneNumber
+    {
+        get;
+        set;
+    }
+    public User(string phoneumber)
+    {
+        this.PhoneNumber = @"^[9]{1}[1]{1}[ ][1-9]{1}[0-9]{9}$";
+    }
+    public void printPhoneNumber()
+    {
+        Console.WriteLine("Phone Number is :{0}", this.PhoneNumber);
+    }
+}
